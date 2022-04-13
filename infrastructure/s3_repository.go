5d45eb699fefdeead941fd7b822afe8ac0c39d0a
kobayashi-m42 <m42kobayashi@gmail.com>
2022-04-13 22:36:17 +0900

    lint のエラー箇所追加

--- infrastructure/s3_repository.go
+++ infrastructure/s3_repository.go
@@ -2,9 +2,9 @@ package infrastructure
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/nekochans/lgtm-cat-api/domain"
