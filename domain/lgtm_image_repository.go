514228012cdf0e5fdc93bd964e50f01c684f2bfb
kobayashi-m42 <m42kobayashi@gmail.com>
2023-01-08 18:27:19 +0900

    不要になったエラーの定義を削除

--- domain/lgtm_image_repository.go
+++ domain/lgtm_image_repository.go
@@ -2,20 +2,10 @@ package domain
 
 import (
 	"context"
-	"fmt"
 )
 
 type LgtmImageRepository interface {
 	FindAllIds(context.Context) (allIds []int32, err error)
 	FindByIds(context.Context, []int32) (lgtmImageObjects []LgtmImageObject, err error)
 	FindRecentlyCreated(context.Context, int) (lgtmImageObjects []LgtmImageObject, err error)
-}
-
-type LgtmImageError struct {
-	Op  string
-	Err error
-}
-
-func (e *LgtmImageError) Error() string {
-	return fmt.Sprintf("lgtmImageRepository: %s, %s", e.Op, e.Err)
 }
