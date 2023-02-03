c6d6924c34e4b00db35388a4ffc32f9af18cc538
kobayashi-m42 <m42kobayashi@gmail.com>
2023-02-03 12:52:14 +0900

    net/htttp で Sentry を利用するために sentry-go/http を追加

--- handler/router.go
+++ handler/router.go
@@ -37,6 +37,7 @@ func NewRouter(uploader *manager.Uploader, q *db.Queries, logger infrastructure.
 	}))
 	r.Use(withLogger(logger))
 	r.Use(recovery)
+	r.Use(infrastructure.NewSentryHttp().Handle)
 
 	r.Get("/health-checks", healthCheckHandler.Check)
 	r.Post("/lgtm-images", createLgtmImageHandler.Create)
---
+++ infrastructure/sentry.go
@@ -0,0 +1,37 @@
+package infrastructure
+
+import (
+	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	sentryhttp "github.com/getsentry/sentry-go/http"
+
+	"github.com/nekochans/lgtm-cat-api/derrors"
+)
+
+const flushTimeSeconds = 2
+
+func InitSentry() (err error) {
+	defer derrors.Wrap(&err, "InitSentry")
+
+	env := os.Getenv("ENV")
+	err = sentry.Init(sentry.ClientOptions{
+		Environment:      env,
+		AttachStacktrace: true,
+		TracesSampleRate: 1.0,
+	})
+	defer sentry.Flush(flushTimeSeconds * time.Second)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewSentryHttp() *sentryhttp.Handler {
+	return sentryhttp.New(sentryhttp.Options{
+		Repanic: true,
+	})
+}
